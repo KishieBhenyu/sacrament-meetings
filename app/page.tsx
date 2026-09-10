1b8a6341a7e3845d3d8806018aa62f128ce9abaf
KishieBhenyu <kbhenyu@byupathway.edu>
2026-09-10 11:27:36 +0200

    fix image

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,7 @@ export default function HomePage() {
           <Image
             src="/images/mkoba_second.jpg"
             alt="Church meetinghouse used by Mkoba Second Ward"
+
             width={1200}
             height={800}
             className="h-auto w-full object-cover"
